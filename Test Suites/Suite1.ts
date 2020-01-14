e90f56bd0ed3e33b304dfbacfff64d0157177052
yi-git <berdyhojayev@gmail.com>
2020-01-13 23:14:23 -0600

    added test suite

---
+++ Test Suites/Suite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>bf27a014-cf99-4c5b-b528-d8d35b01a844</testSuiteGuid>
+   <testCaseLink>
+      <guid>80181224-a903-49e7-8d55-de9f98c2adf6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Simplest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
